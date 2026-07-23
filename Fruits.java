e7c6ff5809c3bd2b453a3c4b6d862116d5d3ca2d
vtu30320-hue <vtu30320@veltech.edu.in>
2026-07-23 22:22:34 +0530

    Delete Fruits.java

--- Fruits.java
+++
@@ -1,9 +0,0 @@
-public class Fruits {
-    public static void main(String[] args) {
-        String[] fruits = {"Apple", "Banana", "Cherry", "Mango"};
-
-        int index = 2;
-
-        System.out.println("Element at index " + index + " is: " + fruits[index]);
-    }
-}
