dc5a431d8de828f085791c267956c7dde19c9358
B.Srishank Reddy <srishankreddybogala@gmail.com>
2026-07-23 22:02:37 +0530

    Completed Week 1 Task 2

---
+++ Fruits.java
@@ -0,0 +1,9 @@
+public class Fruits {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Cherry", "Mango"};
+
+        int index = 2;
+
+        System.out.println("Element at index " + index + " is: " + fruits[index]);
+    }
+}
